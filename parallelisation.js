edc82909e7597ca4223e4a5acc5bf5117acc7bd7
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2016-10-04 11:49:17 +0200

    Ajout des algos de PLL

---
+++ parallelisation.js
@@ -0,0 +1,37 @@
+function nav()
+{
+	document.write('<nav>');
+	document.write('	<section>');
+	document.write('		<div class="en-tete">Liste des pages du site</div>');
+	document.write('		<ul>');
+	document.write('			<li><a href="index.html">Accueil</a></li>');
+	document.write('			<li><a href="pll_simple.html">PLL simples</a></li>');
+	document.write('			<li><a href="oll.html">OLL</a></li>');
+	document.write('			<li><a href="pll_angle.html">PLL sous tous les angles</a></li>');
+	document.write('			<li><a href="ell.html">ELL</a></li>');
+	document.write('			<li><a href="cll.html">CLL</a></li>');
+	document.write('			<li><a href="wv.html">WV</a></li>');
+	document.write('			<li><a href="sv.html">SV</a></li>');
+	document.write('			<li><a href="mw.html">MW</a></li>');
+	document.write('			<li><a href="pll_oh.html">PLL OH</a></li>');
+	document.write('			<li><a href="pll_big_cube.html">PLL Big Cubes</a></li>');
+	document.write('			<li><a href="22ll.html">22LL</a></li>');
+	document.write('		</ul>');
+	document.write('	</section>');
+	document.write('	<section>');
+	document.write('		<div class="en-tete">Autres</div>');
+	document.write('		<ul>');
+	document.write('			<li><a href="#">M\'envoyer un MP</a></li>');
+	document.write('			<li><a href="cube_generator.html">Générateur d\'images de cube</a></li>');
+	document.write('		</ul>');
+	document.write('	</section>');
+	document.write('</nav>');
+}
+
+function footer()
+{
+document.write('<footer>');
+document.write('Ce site est uniquement un endroit où j\'entrepose mes algos de cube, et aura uniquement ce but.<br/>');
+document.write('De ce fait il ne remplace en aucun cas La Tête Dans Le Cube qui est un site visant à centraliser toutes les connaissances sur le cube.');
+document.write('</footer>');
+}
