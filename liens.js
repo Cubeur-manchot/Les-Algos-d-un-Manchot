76a26a3ee7fb7ad941170b7645f66edf8f8b6619
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2016-11-22 12:47:37 +0100

    Update avec ajout des WV
    Attention : il reste des trucs à modifier sur les derniers cas (et les réorganiser).

--- liens.js
+++ liens.js
@@ -10,13 +10,12 @@ function liens()
 		var step = algos[i].getAttribute('data-set');
 		switch(step) // syntaxe à revoir
 		{
-			case 'PLL' : step = 'pll'; break
+			case 'PLL', 'ELL' : step = 'pll'; break
 			case 'OLL' : step = 'oll'; break
-			case 'COLL', 'OLLCP', 'CMLL' : step = 'coll'; break
-			case 'ELL' : step = 'ell'; break
+			case 'WV' : step = 'wv'; break
 		}
 		var html = '<div class="nom">' + nom + '</div>';
-		html += '<img src="http://cube.crider.co.uk/visualcube.php?fmt=svg&size=150&view=plan&case=' + algo + '&step=' + step + '"/>';
+		html += '<img src="http://cube.crider.co.uk/visualcube.php?fmt=svg&size=150&view=plan&case=' + algo + '&stage=' + step + '"/>';
 		html += '<div class="algo" data-set="' + step + '">' + algo + '</div>';
 		html += '<a href="http://alg.cubing.net/?alg=' + algo + '&setup=(' + algo + ')\'" class="bouton_animation">Animation</a>';
 		cas[i].innerHTML = html;
