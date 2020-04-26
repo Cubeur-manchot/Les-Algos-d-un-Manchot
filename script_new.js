538e6baa414b94938fa54e5f10e83ca387084dac
Cubeur-manchot <matthieu.aubert17@gmail.com>
2020-04-27 01:21:27 +0200

    Deal with subsets + add ELL

--- script_new.js
+++ script_new.js
@@ -15,27 +15,47 @@ function loadXml() // load 3x3algs.xml and store it in window.xml3x3algs
 
 function generatePage(setName) // generates the page corresponding to the chosen set
 {
-	let ulHtmlTag = document.querySelector("ul.liste_cas_simple");
+	let algZoneHtmlTag = createHtmlTagWithId("section", "algZone");
 	for (let set of window.xml3x3algs.querySelector("sets").querySelectorAll("set")) {
 		if (set.getAttribute("name") === setName) {
-			for (let algCase of set.getAttribute("caseList").split(" ")) {
-				for (let alg of window.xml3x3algs.querySelector("algs").querySelectorAll("alg")) {
-					if (alg.getAttribute("id") === algCase) {
-						ulHtmlTag.appendChild(makeAlgCard(alg));
-						break;
-					}
+			let caseList = set.getAttribute("caseList");
+			if (caseList === null) { // look for subsets
+				for (let subset of set.querySelectorAll("subset")) {
+					algZoneHtmlTag.appendChild(createHtmlTagWithTextContent("h2", subset.getAttribute("name")));
+					algZoneHtmlTag.appendChild(generatePageForSetsOrSubsets(subset));
 				}
+			} else {
+				algZoneHtmlTag.appendChild(generatePageForSetsOrSubsets(set));
 			}
+			document.querySelector("h1").insertAdjacentElement("afterend", algZoneHtmlTag);
 			return;
 		}
 	}
 }
 
-function makeAlgCard(alg)
+function generatePageForSetsOrSubsets(setOrSubset)
+{
+	let ulHtmlTag = createHtmlTagWithClassName("ul", "liste_cas_simple");
+	let keepAUF = setOrSubset.getAttribute("keepAUF");
+	for (let algCase of setOrSubset.getAttribute("caseList").split(" ")) {
+		for (let alg of window.xml3x3algs.querySelector("algs").querySelectorAll("alg")) {
+			if (alg.getAttribute("id") === algCase) {
+				ulHtmlTag.appendChild(makeAlgCard(alg, keepAUF));
+				break;
+			}
+		}
+	}
+	return ulHtmlTag;
+}
+
+function makeAlgCard(alg, keepAUF)
 {
 	let liHtmlTag = createHtmlTagWithClassName("li", "cas");
 	liHtmlTag.appendChild(createHtmlTagWithClassNameAndTextContent("div", "nom", alg.getAttribute("name")));
 	let moveSequence = alg.getAttribute("sequence");
+	if (keepAUF !== "true") {
+		moveSequence = moveSequence.replace(/ \(.*\)/,""); // remove things in parenthesis
+	}
 	let aHtmlTag = createHtmlTagWithClassNameHrefAndTextContent("a", "bouton_animation",
 		"http://alg.cubing.net/?puzzle=3x3x3&alg=" + moveSequence + "&setup=z2 (" + moveSequence + ")'");
 	aHtmlTag.title = "Animation";
