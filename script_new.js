148e11b9387f61d28cdea8a444b0f24189ca6c15
Cubeur-manchot <matthieu.aubert17@gmail.com>
2020-06-13 00:30:32 +0200

    Add big cube images when needed

--- script_new.js
+++ script_new.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function loadXml() // load 3x3algs.xml and store it in window.xml3x3algs
+function loadXml() // load algs.xml and store it in window.xml3x3algs
 {
 	let xmlHttp;
 	if(window.XMLHttpRequest) {
@@ -8,17 +8,18 @@ function loadXml() // load 3x3algs.xml and store it in window.xml3x3algs
 	} else {
 		xmlHttp = new ActiveXObject("Microsoft.xmlHttp");
 	}
-	xmlHttp.open("get", "3x3algs.xml", false);
+	xmlHttp.open("get", "algs.xml", false);
 	xmlHttp.send();
-	window.xml3x3algs = xmlHttp.responseXML.querySelector("xml");
+	window.xmlalgs = document.querySelector("xml");
 }
 
 function generatePage(setName) // generates the page corresponding to the chosen set
 {
 	let algZoneHtmlTag = createHtmlTagWithId("section", "algZone");
-	for (let set of window.xml3x3algs.querySelector("sets").querySelectorAll("set")) {
+	for (let set of window.xmlalgs.querySelector("sets").querySelectorAll("set")) {
 		if (set.getAttribute("name") === setName) {
 			let options = set.getAttribute("options");
+			let pzl = set.getAttribute("pzl");
 			let useAlternativeName = set.getAttribute("useAlternativeName") === "true";
 			if (options === null) {
 				options = "";
@@ -27,10 +28,10 @@ function generatePage(setName) // generates the page corresponding to the chosen
 			if (caseList === null) { // look for subsets
 				for (let subset of set.querySelectorAll("subset")) {
 					algZoneHtmlTag.appendChild(createHtmlTagWithTextContent("h2", subset.getAttribute("name")));
-					algZoneHtmlTag.appendChild(generatePageForSetsOrSubsets(subset, useAlternativeName, options));
+					algZoneHtmlTag.appendChild(generatePageForSetsOrSubsets(subset, useAlternativeName, options, pzl));
 				}
 			} else {
-				algZoneHtmlTag.appendChild(generatePageForSetsOrSubsets(set, useAlternativeName, options));
+				algZoneHtmlTag.appendChild(generatePageForSetsOrSubsets(set, useAlternativeName, options, pzl));
 			}
 			document.querySelector("h1").insertAdjacentElement("afterend", algZoneHtmlTag);
 			return;
@@ -38,14 +39,14 @@ function generatePage(setName) // generates the page corresponding to the chosen
 	}
 }
 
-function generatePageForSetsOrSubsets(setOrSubset, useAlternativeName, options)
+function generatePageForSetsOrSubsets(setOrSubset, useAlternativeName, options, pzl)
 {
 	let ulHtmlTag = createHtmlTagWithClassName("ul", "liste_cas_simple");
 	let keepAUF = setOrSubset.getAttribute("keepAUF");
 	for (let algCase of setOrSubset.getAttribute("caseList").split(" ")) {
-		for (let alg of window.xml3x3algs.querySelector("algs").querySelectorAll("alg")) {
+		for (let alg of window.xmlalgs.querySelector("algs").querySelectorAll("alg")) {
 			if (alg.getAttribute("id") === algCase) {
-				ulHtmlTag.appendChild(makeAlgCard(alg, keepAUF, useAlternativeName, options));
+				ulHtmlTag.appendChild(makeAlgCard(alg, keepAUF, useAlternativeName, options, pzl));
 				break;
 			}
 		}
@@ -53,7 +54,7 @@ function generatePageForSetsOrSubsets(setOrSubset, useAlternativeName, options)
 	return ulHtmlTag;
 }
 
-function makeAlgCard(alg, keepAUF, useAlternativeName, options)
+function makeAlgCard(alg, keepAUF, useAlternativeName, options, pzl)
 {
 	let liHtmlTag = createHtmlTagWithClassName("li", "cas");
 	let name = alg.getAttribute("name");
@@ -65,12 +66,25 @@ function makeAlgCard(alg, keepAUF, useAlternativeName, options)
 	if (keepAUF !== "true") {
 		moveSequence = moveSequence.replace(/ \(U.*\)/,""); // remove things in parenthesis
 	}
+	let moveSequenceForImage = moveSequence;
+	if (pzl === "5") { // replace wide moves
+		moveSequenceForImage = moveSequenceForImage
+			.replace(/r/g,"4Rw")
+			.replace(/l/g, "4Lw")
+			.replace(/M /g, "4Lw L' ")
+			.replace(/M$/g, "4Lw L'")
+			.replace(/M'/g, "4Lw' L")
+			.replace(/M2'?/g, "4Lw2 L2");
+	} else {
+		pzl = "3";
+	}
 	let aHtmlTag = createHtmlTagWithClassNameHrefAndTextContent("a", "bouton_animation",
 		"http://alg.cubing.net/?puzzle=3x3x3&alg=" + moveSequence + "&setup=z2 (" + moveSequence + ")'");
 	aHtmlTag.title = "Animation";
 	aHtmlTag.target = "_blank";
 	let imgHtmlTag = createHtmlTag("img");
-	imgHtmlTag.src = "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=150&view=plan&bg=white&pzl=3&case=" + moveSequence + options;
+	imgHtmlTag.src = "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=150&view=plan&bg=white&pzl=" + pzl
+		+ "&case=" + moveSequenceForImage + options;
 	aHtmlTag.appendChild(imgHtmlTag);
 	liHtmlTag.appendChild(aHtmlTag);
 	if (moveSequence.length > 26) {
