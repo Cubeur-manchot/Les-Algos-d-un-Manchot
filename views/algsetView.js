f9e79b6b4c535545a1b551da37cf098b259b7cb3
Cubeur-manchot <matthieu.aubert17@gmail.com>
2024-09-09 09:15:26 +0200

    Algset view : move alg logic and fetchs to specific service

--- views/algsetView.js
+++ views/algsetView.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { AlgService } from "../services/algService.js";
 import { HoloCubeService } from "../services/holoCubeService.js";
 import { View } from "./view.js";
 
@@ -14,10 +15,11 @@ class AlgsetView extends View {
 		super();
 		[this.event, this.setName] = restOfThePath.map(pathChunk => pathChunk.replace(/^\//, ""));
 		this.holoCubeService = new HoloCubeService();
+		this.algService = new AlgService();
 	};
 	getTitle = () => [this.event, this.setName].join(" ");
 	getContent = async () => {
-		let algset = await this.fetchAlgset();
+		let algset = await this.algService.fetchData(this.event, this.setName);
 		if (!algset) { // algset not found
 			return [
 				this.createPTag({lang: "en", textContent: "Algset not found."}),
@@ -25,24 +27,19 @@ class AlgsetView extends View {
 			];
 		}
 		this.holoCubeService.buildHoloCubeRunner(AlgsetView.events[this.event].holoCubePuzzleName, algset.mask);
-		let algs = await this.fetchAlgs();
-		let caseCount = algset.subsets
-			.map(subset => subset.caseList.length)
-			.reduce((total, num) => total + num, 0);
-		let subtitleWithCaseCount = [AlgsetView.events[this.event].titleName ?? this.event, caseCount].join (" - ");
 		return [
 			this.createHgroupTag({id: "main-title"},
 				this.createH1Tag({textContent: [algset.name, algset.detailedName].filter(Boolean).join(" - ")}),
-				this.createSpanTag({lang: "en", className: "subtitle", textContent: `${subtitleWithCaseCount} cases`}),
-				this.createSpanTag({lang: "fr", className: "subtitle", textContent: `${subtitleWithCaseCount} cas`})
+				this.createSpanTag({lang: "en", className: "subtitle", textContent: `${AlgsetView.events[this.event].titleName ?? this.event} - ${algset.caseCount} cases`}),
+				this.createSpanTag({lang: "fr", className: "subtitle", textContent: `${AlgsetView.events[this.event].titleName ?? this.event} - ${algset.caseCount} cas`})
 			),
 			...algset.subsets.map(subset =>
 				this.createSectionTag({id: subset.name},
 					this.createH2Tag({textContent: subset.name}),
 					this.createUlTag({className: "cardList algCardList"},
 						...subset.caseList
-							.map(algCase => algs.find(alg => alg.id === algCase))
-							.map(alg => Object.assign(alg, {algToUse: this.getAlgToUse(alg)}))
+							.map(algCase => this.algService.algs.find(alg => alg.id === algCase))
+							.map(alg => Object.assign(alg, {algToUse: this.algService.getAlgToUse(alg, this.event)}))
 							.map(alg =>
 								this.createLiTag({className: "card imageCard algCard"},
 									this.holoCubeService.createHoloCubeImage(alg.algToUse),
@@ -54,27 +51,6 @@ class AlgsetView extends View {
 				)
 			)
 		];
-	};
-	fetchAlgset = () => fetch("../data/algsets.json")
-		.then(response => response.json())
-		.then(algsets => algsets.find(algset => algset.events.includes(this.event) && algset.name === this.setName));
-	fetchAlgs = () => fetch("../data/algs.json")
-		.then(response => response.json())
-		.then(algs =>
-			[
-				...algs["lastLayer"]["1LLL"],
-				...algs["lastLayer"]["others"],
-				...algs["others"]
-			]
-		);
-	getAlgToUse = alg => {
-		if (this.event === "3x3" || !alg.alt) {
-			return alg.alg; // default alg
-		}
-		switch (typeof alg.alt) {
-			case "string": return alg.alt; // one alternative alg for all variations
-			case "object": return alg.alt[this.event] ?? alg.alg; // alternatives per variation
-		}
 	};
 };
 
