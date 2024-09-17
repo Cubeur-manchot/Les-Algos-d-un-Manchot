5522cc8c2d91b69012ea822afcfa086a2b52facc
Cubeur-manchot <matthieu.aubert17@gmail.com>
2024-09-17 09:03:34 +0200

    Home view : use URLs defined in other views

--- views/homeView.js
+++ views/homeView.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import { ContactView } from "./contactView.js";
+import { CollectionView } from "./collectionView.js";
+import { RecordsView } from "./recordsView.js";
 import { View } from "./view.js";
 
 class HomeView extends View {
@@ -15,10 +18,10 @@ class HomeView extends View {
 			this.createNavTag({className: "cardList navigationCardList"},
 				this.createNavigationCard(false, "Cubeurs Francophones", "Cubeurs Francophones", "../images/logos/discord.svg", "https://discord.gg/MZ75mtHdGZ"),
 				this.createNavigationCard(false, "French UNR List", "Liste UNR de France", "../images/logos/unrList.png", "https://cubeur-manchot.github.io/Liste-UNR-de-France/"),
-				this.createNavigationCard(true, "Records", "Records", "../images/logos/records.png", "/records"),
-				this.createNavigationCard(true, "Collection", "Collection", "../images/logos/collection.png", "/collection"),
+				this.createNavigationCard(true, "Records", "Records", "../images/logos/records.png", RecordsView.path),
+				this.createNavigationCard(true, "Collection", "Collection", "../images/logos/collection.png", CollectionView.path),
 				this.createNavigationCard(false, "My Drive", "Mon Drive", "../images/logos/drive.png","https://drive.google.com/drive/folders/0B23xBbKROAE4fjlDSWNZSFAyUHJvSm1HelpUdHpsMzlMTVdQa0VqdldIbHc5RThnVVFvX2M?usp=sharing"),
-				this.createNavigationCard(true, "Contact", "Contact", "../images/logos/avatar.png","/contact")
+				this.createNavigationCard(true, "Contact", "Contact", "../images/logos/avatar.png", ContactView.path)
 			)
 		];
 	createNavigationCard = (isInternal, titleEnglish, titleFrench, src, href) =>
