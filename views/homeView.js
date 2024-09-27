91ee36c80f30786e8849c30532fede418a180a9a
Cubeur-manchot <matthieu.aubert17@gmail.com>
2024-09-28 01:11:04 +0200

    Home view - Use DataService

--- views/homeView.js
+++ views/homeView.js
@@ -1,12 +1,12 @@
 "use strict";
 
-import { AlgService } from "../services/algService.js";
 import { AlgsetView } from "./algsetView.js";
 import { ContactView } from "./contactView.js";
 import { CollectionView } from "./collectionView.js";
-import { HoloCubeService } from "../services/holoCubeService.js";
 import { RecordsView } from "./recordsView.js";
 import { View } from "./view.js";
+import { DataService } from "../services/dataService.js";
+import { HoloCubeService } from "../services/holoCubeService.js";
 
 class HomeView extends View {
 	static path = "/home";
@@ -14,19 +14,18 @@ class HomeView extends View {
 	constructor() {
 		super();
 		this.holoCubeService = new HoloCubeService();
-		this.algService = new AlgService();
 	};
 	getContent = async () => {
-		await this.algService.fetchData();
+		await this.dataService.getManyJson(["algs", "algsets"]);
 		let algsetsGroupedByEvent = this.groupByEventName(
-			this.algService.algsets
+			this.dataService.algsets
 			.map(algset => algset.events.map(event => Object.assign({event}, algset)))
 			.flat()
 		);
 		return [
 			...algsetsGroupedByEvent.map(algsetsGroup =>
 				[
-					this.createH2Tag({textContent: AlgService.getEventTitleName([algsetsGroup.event])}),
+					this.createH2Tag({textContent: DataService.getEventTitleName([algsetsGroup.event])}),
 					this.createNavTag({className: "cardList navigationCardList"},
 						...algsetsGroup.algsets.map(algset =>
 							this.createAlgNavigationCard(algset)
@@ -37,12 +36,12 @@ class HomeView extends View {
 			this.createH2Tag({textContent: "Others", lang: "en"}),
 			this.createH2Tag({textContent: "Autres", lang: "fr"}),
 			this.createNavTag({className: "cardList navigationCardList"},
-				this.createNavigationCard(false, "Cubeurs Francophones", "Cubeurs Francophones", "../images/logos/discord.svg", "https://discord.gg/MZ75mtHdGZ"),
-				this.createNavigationCard(false, "French UNR List", "Liste UNR de France", "../images/logos/unrList.png", "https://cubeur-manchot.github.io/Liste-UNR-de-France/"),
-				this.createNavigationCard(true, "Records", "Records", "../images/logos/records.png", RecordsView.path),
-				this.createNavigationCard(true, "Collection", "Collection", "../images/logos/collection.png", CollectionView.path),
-				this.createNavigationCard(false, "My Drive", "Mon Drive", "../images/logos/drive.png","https://drive.google.com/drive/folders/0B23xBbKROAE4fjlDSWNZSFAyUHJvSm1HelpUdHpsMzlMTVdQa0VqdldIbHc5RThnVVFvX2M?usp=sharing"),
-				this.createNavigationCard(true, "Contact", "Contact", "../images/logos/avatar.png", ContactView.path)
+				this.createNavigationCard(false, "Cubeurs Francophones", "Cubeurs Francophones", "./images/logos/discord.svg", "https://discord.gg/MZ75mtHdGZ"),
+				this.createNavigationCard(false, "French UNR List", "Liste UNR de France", "./images/logos/unrList.png", "https://cubeur-manchot.github.io/Liste-UNR-de-France/"),
+				this.createNavigationCard(true, "Records", "Records", "./images/logos/records.png", RecordsView.path),
+				this.createNavigationCard(true, "Collection", "Collection", "./images/logos/collection.png", CollectionView.path),
+				this.createNavigationCard(false, "My Drive", "Mon Drive", "./images/logos/drive.png","https://drive.google.com/drive/folders/0B23xBbKROAE4fjlDSWNZSFAyUHJvSm1HelpUdHpsMzlMTVdQa0VqdldIbHc5RThnVVFvX2M?usp=sharing"),
+				this.createNavigationCard(true, "Contact", "Contact", "./images/logos/avatar.png", ContactView.path)
 			)
 		];
 	};
@@ -56,7 +55,7 @@ class HomeView extends View {
 		this.holoCubeService.buildHoloCubeRunner(HoloCubeService.holoCubePuzzleNames[algset.event], algset.mask);
 		return this.createATag({className: "card imageCard navigationCard internalNavigation", href: `${AlgsetView.path}/${algset.event}/${algset.name}`},
 			this.holoCubeService.createHoloCubeImage(
-				this.algService.getAlgToUse(this.algService.findAlg(algset.exampleCase), algset.event)
+				this.dataService.findAlg(algset.exampleCase).alg
 			),
 			this.createSpanTag({className: "navigationCard__title", textContent: algset.name})
 		);
