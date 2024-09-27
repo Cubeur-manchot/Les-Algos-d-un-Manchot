ee26368084ddbea29c890ea903aa5bbf4ebb9fdf
Cubeur-manchot <matthieu.aubert17@gmail.com>
2024-09-28 01:10:20 +0200

    ContactView - Move data to separate JSON and use DataService

--- views/contactView.js
+++ views/contactView.js
@@ -8,8 +8,9 @@ class ContactView extends View {
 	constructor() {
 		super();
 	};
-	getContent = () =>
-		[
+	getContent = async () => {
+		let contacts = await this.dataService.getJson("contacts");
+		return [
 			this.createH1Tag({textContent: "Contact"}),
 			this.createDivTag({className: "table-outer-container"},
 				this.createDivTag({className: "table-inner-container"},
@@ -24,59 +25,15 @@ class ContactView extends View {
 							)
 						),
 						this.createTbodyTag({},
-							this.createTrTag({},
-								this.createTdTag({textContent: "Discord"}),
-								this.createTdTag({textContent: "Cubeur-manchot (@cubeurmanchot)"}),
-								this.createTdTag({},
-									this.createATag({href: "https://discord.com/channels/329175643877015553"},
-										this.createSpanTag({lang: "en", textContent: "Cubeurs Francophones (Discord server)"}),
-										this.createSpanTag({lang: "fr", textContent: "Cubeurs Francophones (serveur Discord)"})
-									)
-								)
-							),
-							this.createTrTag({},
-								this.createTdTag({lang: "en", textContent: "Francocube forum"}),
-								this.createTdTag({lang: "fr", textContent: "Forum Francocube"}),
-								this.createTdTag({textContent: "Cubeur-manchot"}),
-								this.createTdTag({},
-									this.createATag({href: "https://forum.francocube.com/ucp.php?i=pm&mode=compose&u=4736"},
-										this.createSpanTag({lang: "en", textContent: "DM on Francocube"}),
-										this.createSpanTag({lang: "fr", textContent: "MP sur Francocube"})
-									)
-								)
-							),
-							this.createTrTag({},
-								this.createTdTag({textContent: "GitHub"}),
-								this.createTdTag({textContent: "Cubeur-manchot"}),
-								this.createTdTag({},
-									this.createATag({href: "https://github.com/Cubeur-manchot"},
-										this.createSpanTag({lang: "en", textContent: "GitHub profile"}),
-										this.createSpanTag({lang: "fr", textContent: "Profil GitHub"})
-									)
-								)
-							),
-							this.createTrTag({},
-								this.createTdTag({textContent: "Gmail"}),
-								this.createTdTag({textContent: "cubeurmanchot@gmail.com"}),
-								this.createTdTag({textContent: "-"})
-							),
-							this.createTrTag({},
-								this.createTdTag({textContent: "Instagram"}),
-								this.createTdTag({textContent: "Matthieu Aubert (@cubeur_manchot)"}),
-								this.createTdTag({},
-									this.createATag({href: "https://www.instagram.com/cubeur_manchot/"},
-										this.createSpanTag({lang: "en", textContent: "Instagram profile"}),
-										this.createSpanTag({lang: "fr", textContent: "Profil Instagram"})
-									)
-								)
-							),
-							this.createTrTag({},
-								this.createTdTag({textContent: "YouTube"}),
-								this.createTdTag({textContent: "Matthieu Aubert (@MatthieuAubert)"}),
-								this.createTdTag({},
-									this.createATag({href: "https://www.youtube.com/@MatthieuAubert"},
-										this.createSpanTag({lang: "en", textContent: "YouTube channel"}),
-										this.createSpanTag({lang: "fr", textContent: "Chaîne YouTube"})
+							...contacts.map(contact =>
+								this.createTrTag({},
+									this.createTdTag({textContent: contact.platform}),
+									this.createTdTag({textContent: contact.pseudo}),
+									this.createTdTag({},
+										this.createATag({href: contact.link.href},
+											this.createSpanTag({lang: "en", textContent: contact.link.text.en}),
+											this.createSpanTag({lang: "fr", textContent: contact.link.text.fr})
+										)
 									)
 								)
 							)
@@ -85,6 +42,7 @@ class ContactView extends View {
 				)
 			)
 		];
+	};
 };
 
 export {ContactView};
