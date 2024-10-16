495cd140a8365452d4285be1e3ef41a736b32dc8
Cubeur-manchot <matthieu.aubert17@gmail.com>
2024-10-17 00:28:00 +0200

    Home view : fix image link, new try

--- views/homeView.js
+++ views/homeView.js
@@ -37,12 +37,12 @@ class HomeView extends View {
 			this.createH2Tag({textContent: "Others", lang: "en"}),
 			this.createH2Tag({textContent: "Autres", lang: "fr"}),
 			this.createNavTag({className: "cardList navigationCardList"},
-				this.createNavigationCard(false, "Cubeurs Francophones", "Cubeurs Francophones", "./images/logos/discord.svg", "https://discord.gg/MZ75mtHdGZ"),
-				this.createNavigationCard(false, "French UNR List", "Liste UNR de France", "./images/logos/unrList.png", "https://cubeur-manchot.github.io/Liste-UNR-de-France/"),
-				this.createNavigationCard(true, "Records", "Records", "./images/logos/records.png", RecordsView.path),
-				this.createNavigationCard(true, "Collection", "Collection", "./images/logos/collection.png", CollectionView.path),
-				this.createNavigationCard(false, "My Drive", "Mon Drive", "./images/logos/drive.png","https://drive.google.com/drive/folders/0B23xBbKROAE4fjlDSWNZSFAyUHJvSm1HelpUdHpsMzlMTVdQa0VqdldIbHc5RThnVVFvX2M?usp=sharing"),
-				this.createNavigationCard(true, "Contact", "Contact", "./images/logos/avatar.png", ContactView.path)
+				this.createNavigationCard(false, "Cubeurs Francophones", "Cubeurs Francophones", "/images/logos/discord.svg", "https://discord.gg/MZ75mtHdGZ"),
+				this.createNavigationCard(false, "French UNR List", "Liste UNR de France", "/images/logos/unrList.png", "https://cubeur-manchot.github.io/Liste-UNR-de-France/"),
+				this.createNavigationCard(true, "Records", "Records", "/images/logos/records.png", RecordsView.path),
+				this.createNavigationCard(true, "Collection", "Collection", "/images/logos/collection.png", CollectionView.path),
+				this.createNavigationCard(false, "My Drive", "Mon Drive", "/images/logos/drive.png","https://drive.google.com/drive/folders/0B23xBbKROAE4fjlDSWNZSFAyUHJvSm1HelpUdHpsMzlMTVdQa0VqdldIbHc5RThnVVFvX2M?usp=sharing"),
+				this.createNavigationCard(true, "Contact", "Contact", "/images/logos/avatar.png", ContactView.path)
 			)
 		];
 	};
